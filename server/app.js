4b2b2b93278cf08d4360da9bf9fe2781ff111f2f
boazfr1 <boazfr1@gmail.com>
2023-01-25 12:48:17 +0200

    v1

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const fs = require("fs")
+const data = require('./DB/database.json')
+
 
 
 
@@ -22,29 +24,70 @@ function createDB(name) {
     })
 }
 // createDB('JSON_placeholder')
-
-function readAndCreate(fileName) {
-    console.log(fileName)
-    fs.readFile(`./DB/${fileName}.json`, (err, data) => {
-        if (err) console.log(err);
-        let myData = JSON.parse(data.toString())
-        con.connect(function (err) {
-            if (err) throw err;
-            let sqlTableName = `CREATE TABLE ${myData.tableName} `;
-            for (let i = 0; i < myData.fields.length; i++) {
-                let sqlCommand = ` ${myData.fields[i].name + " " + myData.fields[i].type}`
-                con.query(sqlTableName + "(" + sqlCommand + ")", function (err, result) {
-                    if (err) console.log(err);
-                    console.log(result)
-                });
+function createTables() {
+    con.connect((err) => {
+        if (err) throw err;
+        console.log('connected');
+        for (const key in data) {
+            let titles = '';
+            for (const k in data[key]) {
+                titles += `${k} ${data[key][k]},`
             }
-        })
+            titles = titles.slice(0, titles.length - 1)
+            let sql = `CREATE TABLE ${key} (${titles});`
+    
+            con.query(sql, (err, result, fields) => {
+                if (err) throw err;
+                console.log(result);
+            })
+        }
     });
 }
+createTables();
 
-function insertManeger() {
-    let sqlCommand = "INSERT INTO admin (admin_name, admin_password, school_id) VALUES ('david', 'david1234', 1),('aharon', 'aharon1234', 2)"
-    con.query(sqlCommand, function (err, result) {
-        if (err) console.log(err);
-    });
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function readAndCreate(fileName) {
+//     console.log(fileName)
+//     fs.readFile(`./DB/${fileName}.json`, (err, data) => {
+//         if (err) console.log(err);
+//         let myData = JSON.parse(data.toString())
+//         con.connect(function (err) {
+//             if (err) throw err;
+//             let sqlTableName = `CREATE TABLE ${myData.tableName} `;
+//             for (let i = 0; i < myData.fields.length; i++) {
+//                 let sqlCommand = ` ${myData.fields[i].name + " " + myData.fields[i].type}`
+//                 con.query(sqlTableName + "(" + sqlCommand + ")", function (err, result) {
+//                     if (err) console.log(err);
+//                     console.log(result)
+//                 });
+//             }
+//         })
+//     });
+// }
+
+// function insertManeger() {
+//     let sqlCommand = "INSERT INTO admin (admin_name, admin_password, school_id) VALUES ('david', 'david1234', 1),('aharon', 'aharon1234', 2)"
+//     con.query(sqlCommand, function (err, result) {
+//         if (err) console.log(err);
+//     });
+// }
