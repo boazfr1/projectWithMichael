d3a1ec74447ce4c29714ae880aa90f0a9423c9d6
Michael <Michaelsalem720@gmail.com>
2023-01-26 16:38:33 +0200

    v4

--- client/src/component/ToDos.js
+++ client/src/component/ToDos.js
@@ -1,7 +1,5 @@
-// import { userInfo } from "os";
 import { useContext, useEffect, useState } from "react";
 import { userInfoContext } from "./userInfoContext";
-
 
 function ToDos() {
 
@@ -9,7 +7,6 @@ function ToDos() {
     let [sortBy, setSortBy] = useState('id')
     useEffect(() => {
         fetchToDos()
-        // return ()=>{postTodos()}//unmount
     }, [])
     useEffect(() => {
         console.log(sortBy);
@@ -17,40 +14,22 @@ function ToDos() {
     }, [sortBy])
 
     window.addEventListener('beforeunload', postTodos)
-    // useEffect(() => {
-    //     sortData(toDoArray)
-    // }, [toDoArray])
 
     let userInfo = useContext(userInfoContext)
     async function fetchToDos() {
-        // if (localStorage.getItem('toDos') === null || localStorage.getItem('toDos') === undefined) {
-        // const userId = JSON.parse(localStorage.getItem('currentUser')).id
         let res = await fetch(`http://localhost:3002/todos/${userInfo.myInfo.id}`);
         let toDos = await res.json();
-        console.log(toDos);
-        // localStorage.setItem('toDos', JSON.stringify(toDos))
         setToDoArray(toDos);
-        console.log(toDoArray);
-        // }
-        // else {
-        // let toDos = JSON.parse(localStorage.getItem('toDos'))
-        // setToDoArray(toDos);
-        // }
     };
     let toggleCheckBox = (i) => {
         let toDos = [...toDoArray]
         toDos[i].completed = !toDos[i].completed
         setToDoArray(toDos)
         console.log(i);
-        // localStorage.setItem('toDos', JSON.stringify(toDoArray))
     }
-        
-    
+
     function changeSortBy(e) {
         setSortBy(() => `${e.target.value}`)
-        console.log(sortBy);
-        console.log(e.target.value);
-
     }
 
     function sortData(arr) {
@@ -59,19 +38,17 @@ function ToDos() {
         });
     }
     function postTodos() {
-        console.log('userInfo.myInfo.id: ', userInfo.myInfo.id);
         fetch(`http://localhost:3002/todos/${userInfo.myInfo.id}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(toDoArray)
-            
         })
     }
+
     return (
         <div>
-            <button onClick={postTodos}>onrefresh</button>
             <label>Sort By:</label>
             <select value={sortBy} onChange={changeSortBy} name='sort'>
                 <option value="id">ID</option>
